b74709f347258f9eaa21291c1d49a5a64be658a1
Guy John <d.guyjohn@gmail.com>
2011-08-21 22:56:59 +0100

    Modifying autoload function
    Classes can now be loaded from both the minvc and app folder.
    classes can also use underscores in their names as folder dividers

--- minvc/autoload/autoload.php
+++ minvc/autoload/autoload.php
@@ -3,5 +3,14 @@
 #function to deal with autoloading of classes
 function __autoload($class_name)
 {
-    require_once APP . '/classes/' . $class_name . '.class.php';
+    $class_file = str_replace('_', '/', $class) . '.php';
+    $class_folders = array(APP, MINVC);
+    foreach ($class_folders as $folder)
+    {
+        $class_path = $folder . "classes/" . $class_file;
+        if (file_exists($class_path) && is_readable($class_path))
+        {
+            require_once $class_path;
+        }
+    }
 }
