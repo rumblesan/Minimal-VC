7ca7b2d6919fade04f2b86f16cd38af5450252bf
Guy John <d.guyjohn@gmail.com>
2011-08-26 10:07:24 -0400

    added sidebar entry for class tests

--- app/views/layouts/sidebar.php
+++ app/views/layouts/sidebar.php
@@ -4,6 +4,6 @@
 <li><a href=<?php href('main/test/') ?>>Hello World Test</a></li>
 <li><a href=<?php href('main/test/Foo/25/') ?>>Hello Foo Test</a></li>
 <li><a href=<?php href('loading/') ?>>AutoLoad Test</a></li>
-<li><a href=<?php href('classtest//') ?>>Class Tests</a></li>
+<li><a href=<?php href('classtest/index/') ?>>Class Tests</a></li>
 </ul>
 
