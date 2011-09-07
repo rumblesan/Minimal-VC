f14a499ad784b03ca5706f883258a44c208d79d8
Guy John <d.guyjohn@gmail.com>
2011-09-07 12:36:56 +0100

    fixing links in sidebar template

--- app/templates/layouts/sidebar.php
+++ app/templates/layouts/sidebar.php
@@ -1,9 +1,8 @@
 
 <ul>
 <li><a href=<?php href('main/') ?>>Main</a></li>
-<li><a href=<?php href('main/test/') ?>>Hello World Test</a></li>
-<li><a href=<?php href('main/test/Foo/25/') ?>>Hello Foo Test</a></li>
-<li><a href=<?php href('loading/') ?>>AutoLoad Test</a></li>
-<li><a href=<?php href('classtest/index/') ?>>Class Tests</a></li>
+<li><a href=<?php href('tests/args/') ?>>Hello World Test</a></li>
+<li><a href=<?php href('tests/args/Foo/25/') ?>>Hello Foo Test</a></li>
+<li><a href=<?php href('tests/loading/') ?>>AutoLoad Test</a></li>
 </ul>
 
