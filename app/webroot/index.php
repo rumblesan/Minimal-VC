0e057c405453603c22c07c84938051e6134dc906
Guy John <d.guyjohn@gmail.com>
2011-09-26 09:07:59 +0100

    Updating the main index file
    Added some comments and cleaned a few bits up

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -1,35 +1,62 @@
 <?php
 
-    # BASE is the directory that Minimal VC is installed to
-    # this is removed from the request URL to get the correct path
+    /*
+    BASE is the directory that the framework is installed to
+    this is removed from the request URL to get the correct path
+    for example, if it were installed to :-
+        http://foo.com/bar/
+    then BASE need to be set to bar
+    
+    otherwise it needs to be set to a single slash  /
+    */
     define('BASE',  '/');
 
-    # Paths to Minimal VC folder and APP folder
+    /*
+    Paths to framework folder and APP folder
+    */
     define('ROOT',  dirname(dirname(dirname(__FILE__))));
     define('MINVC', ROOT . '/minvc/');
     define('APP',   ROOT . '/app/');
 
-    # Paths to app controllers and views
+    /*
+    Paths to app controllers and views
+    */
     define('CPATH', APP . 'controllers/');
     define('MPATH', APP . 'models/');
     define('VPATH', APP . 'views/');
 
-    # Turn autoloading on or off
+    /*
+    Turn autoloading on or off
+    */
     define('AUTOLOAD', True);
 
-    # Load app config files
+    /*
+    Load app config files
+    */
     require_once APP . 'cfg/cfg.php';
 
-    # Get core Minimal VC files
+    /*
+    Load core file
+    */
     require_once MINVC . 'minvc_core.php';
 
+    /*
+    Setthe include path
+    */
     ini_set("include_path", APP);
 
+    /*
+    The Path object holds the paths to the main folders
+    within the app section
+    Makes it easier to pass them around
+    */
     $paths = New Paths();
     $paths->set_path('controller', CPATH)
           ->set_path('view',       VPATH)
           ->set_path('model',      MPATH);
 
-
-    $controller = New Router($_SERVER['REQUEST_URI'], BASE, 'main', $paths);
+    /*
+    The Router takes the URL, parses it and then runs the correct controller
+    */
+    $router = New Router($_SERVER['REQUEST_URI'], BASE, 'main', $paths);
 
