e9619bbe93e09e7397f59907c93fbe593356d07e
Guy John <d.guyjohn@gmail.com>
2011-10-21 17:52:54 -0400

    Removing last reference to Paths class

--- framework/framework_core.php
+++ framework/framework_core.php
@@ -4,7 +4,6 @@
     requires the core framework classes
     */
     require_once FRAMEWORK . '/classes/Controller.class.php';
-    require_once FRAMEWORK . '/classes/Paths.class.php';
     require_once FRAMEWORK . '/classes/Router.class.php';
     require_once FRAMEWORK . '/classes/Model.class.php';
     require_once FRAMEWORK . '/classes/View.class.php';
